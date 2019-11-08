1b3de236e665d71e88f488cdc0eae2c11994e765
Sohail Ali <soali2019@outlook.com>
2019-11-08 10:22:34 +0530

    Add project files.

---
+++ Prototype/Prototype/Views/HomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prototype.ViewModels;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype.Views
+{
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            Title = "Home Page";
+            (BindingContext as HomePageViewModel).Navigation = Navigation;
+        }
+    }
+}
